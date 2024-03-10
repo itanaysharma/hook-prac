a065410932d9df5ae1841bd65fba529c22b3592e
itanaysharma <thisistanaysharma@gmail.com>
2024-03-10 19:47:03 +0530

    Added custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-function App() {
+function useTodos() {
   const [todos, setTodos] = useState([]);
-
   useEffect(() => {
     axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
       setTodos(res.data.todos);
     });
   }, []);
+  return todos;
+}
 
+function App() {
+  const todos = useTodos();
   return (
     <>
       {todos.map((todo) => (
