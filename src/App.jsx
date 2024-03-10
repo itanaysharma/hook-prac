c11dee6ed71e19efac924e8e7b2fa98ce8a0a40b
itanaysharma <thisistanaysharma@gmail.com>
2024-03-10 20:09:44 +0530

    we can directly use swr libary to do the same https://swr.vercel.app/

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr"; //this can be used directly to do the same job https://swr.vercel.app/
 import axios from "axios";
 function useTodos(n) {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setInterval(() => {
+    const value = setInterval(() => {
       axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
         setTodos(res.data.todos);
         setLoading(false);
       });
     }, n * 1000);
     axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+      // added because we want to show something on the first attempt
       setTodos(res.data.todos);
       setLoading(false);
     });
-  }, []);
+
+    return () => {
+      closeInterval(value);
+    };
+  }, [n]);
   return { todos: todos, loading: loading };
 }
 
