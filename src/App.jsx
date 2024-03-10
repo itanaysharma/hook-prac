5f861f1cb3993e3690362f9f58a0347aaa8d1265
itanaysharma <thisistanaysharma@gmail.com>
2024-03-10 19:55:23 +0530

    added Loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 function useTodos() {
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
       setTodos(res.data.todos);
+      setLoading(false);
     });
   }, []);
-  return todos;
+  return { todos: todos, loading: loading };
 }
 
 function App() {
-  const todos = useTodos();
+  const { todos, loading } = useTodos();
+
+  if (loading) {
+    return <div>Loading ..</div>;
+  }
   return (
     <>
       {todos.map((todo) => (
